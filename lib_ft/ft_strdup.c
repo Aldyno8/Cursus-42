0a9ad9f7dc9fd5156ebf6dd44843ba862c58b176
AldYnot8 <aldynobruel@gmail.com>
2025-03-03 19:44:29 +0300

    atoi, isdigit, strdup, tolower

---
+++ lib_ft/ft_strdup.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/03 19:32:38 by marvin            #+#    #+#             */
+/*   Updated: 2025/03/03 19:32:38 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+
+#include <stdlib.h>
+
+char	*ft_strdup(char *src)
+{
+	int		i;
+	char	*dest;
+
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i ++;
+	}
+	dest = malloc(sizeof(char) * i);
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i ++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
