0dac0283f66692e0ca9cdf3ca41502f94094eaff
Bruel Ydric Aldyno Velonjara <bvelonja@c1r12s11.42antananarivo.mg>
2025-03-25 21:59:27 +0300

    makefile et memcpy

--- lib_ft/ft_memcpy.c
+++ lib_ft/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: bvelonja <bvelonja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/07 09:24:59 by bvelonja          #+#    #+#             */
-/*   Updated: 2025/03/12 13:43:14 by bvelonja         ###   ########.fr       */
+/*   Updated: 2025/03/25 21:58:38 by bvelonja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	unsigned char		*d;
 	const unsigned char	*s;
 
+	if (!dest || !src)
+		return (NULL);
 	d = dest;
 	s = src;
 	while (n > 0)
