5a607efb38c6de787301ba1d669de34cac6e017c
Bruel Ydric Aldyno Velonjara <bvelonja@c1r10s10.42antananarivo.mg>
2025-04-07 10:24:54 +0300

    lstmap

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bvelonja <bvelonja@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/18 09:20:51 by bvelonja          #+#    #+#             */
-/*   Updated: 2025/03/18 10:49:10 by bvelonja         ###   ########.fr       */
+/*   Updated: 2025/04/07 09:59:25 by bvelonja         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,22 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*new_list;
 	t_list	*new_element;
 
-	new_list = NULL;
 	if (!lst || !f || !del)
 		return (NULL);
+	new_list = NULL;
 	while (lst)
 	{
 		new_element = ft_lstnew(f(lst -> content));
-		if (!new_element)
+		if (new_element == NULL)
 		{
-			ft_lstclear(&new_list, del);
+			while (new_list)
+			{
+				new_element = new_list-> next;
+				del(new_list-> content);
+				free(new_list);
+				new_list = new_element;
+			}
+			lst = NULL;
 			return (NULL);
 		}
 		ft_lstadd_back(&new_list, new_element);
